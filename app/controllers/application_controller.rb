a211e366f68c2345c8c05dbe6b318d460d870ed9
Jesse Shawl <jesse@jshawl.com>
2015-05-07 19:47:18 -0400

    token based auth is working
    http://provoost.tumblr.com/post/80873086965/json-api-authentication-using-devise-tokens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  acts_as_token_authentication_handler_for User
+  respond_to :html, :json
+  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  acts_as_token_authentication_handler_for User, fallback_to_devise: false
 end
