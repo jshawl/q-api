d8bcfc5dc9d0a061da7b2eb87860b5442b385fc9
Jesse Shawl <jesse@jshawl.com>
2015-05-11 20:31:59 -0400

    add some more tasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+User.destroy_all
 u = User.create!(email:"j@j.com", authentication_token:"kUZY98qFsbDimxR14Qzn", password: "12345678")
 
 Group.destroy_all
@@ -9,5 +10,11 @@ groups = u.groups.create([
 Task.destroy_all
 groups[0].tasks.create( { name: "Dishes"})
 groups[0].tasks.create( { name: "Recycling"})
+groups[0].tasks.create( { name: "Trash"})
+groups[0].tasks.create( { name: "Clean Downstairs Bathroom"})
+groups[0].tasks.create( { name: "Cut the Grass"})
+groups[0].tasks.create( { name: "Tupperware"})
+groups[0].tasks.create( { name: "Olive Oil"})
+
 groups[1].tasks.create( { name: "Grade Homework"})
 
