7cdb72416fc8176d04947fc58aae772e44a1f54a
Jesse Shawl <jesse@jshawl.com>
2015-05-12 18:23:10 -0400

    add email to events api

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :task
+
+  def as_json(options={})
+    super.as_json(options).merge({user_email: get_user_email})
+  end
+
+  def get_user_email
+    User.find( self.user_id ).email
+  end
+
 end
