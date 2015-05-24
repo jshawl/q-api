156f148c8c36a59cb576ea4d331e60496d9c455f
Jesse Shawl <jesse@jshawl.com>
2015-05-23 20:29:13 -0400

    add mark done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,8 @@ class EventsController < ApplicationController
     @events = task.events
     render json: @events
   end
+  def create
+    @event = current_user.groups.find(params[:group_id]).tasks.find(params[:task_id]).events.create( user_id: current_user.id )
+    render json: @event
+  end
 end
