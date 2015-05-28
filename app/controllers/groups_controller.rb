a49a406b6c865b351593034cb854c2d4a4711037
Jesse Shawl <jesse@jshawl.com>
2015-05-27 20:26:39 -0400

    join group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,10 +8,16 @@ class GroupsController < ApplicationController
     render json: @group
   end
   def create
-    @group = current_user.groups.create group_params
-    if @group
-      render json: @group
+    name = group_params[:name]
+    hashids = Hash
+    hashids = Hashids.new(ENV['hashids_salt'],3,"abcdefghijklmnopqrstuvwxyz1234567890")
+    decoded = hashids.decode(group_params[:name])
+    if decoded.empty?
+      @group = current_user.groups.create group_params
+    else
+      @group = Group.find(decoded[0])
     end
+    render json: @group
   end
   def destroy
     @group = Group.find(params[:id])
